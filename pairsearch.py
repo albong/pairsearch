cabc025bba49b431e0970ae4368ceb382e92c0a0
albong <bongio.alex@gmail.com>
2019-04-28 16:44:43 -0400

    Added my local files to git.

---
+++ pairsearch.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+https://docs.python.org/3/library/tkinter.html
+https://likegeeks.com/python-gui-examples-tkinter-tutorial/
+"""
+import tkinter
+
+window = tkinter.Tk()
+window.title("pairsearch: Exponent pair searcher")
+window.mainloop()
+
